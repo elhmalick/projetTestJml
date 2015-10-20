2e9eb14d4870ffc0b826aaab5dbc6f186acc1147
uizo <umar-ibn-zaid.ozeer@e.ujf-grenoble.fr>
2015-10-20 11:28:00 +0200

    Update Explosives.java

--- src/source/Explosives.java
+++ src/source/Explosives.java
@@ -1,4 +1,3 @@
-package source;
 // Based on a B specification from Marie-Laure Potet.
 
 public class Explosives{
@@ -10,29 +9,42 @@ public class Explosives{
     /*@ public invariant // Prop 1
       @ (0 <= nb_inc && nb_inc < 50);
       @*/
-      //le nombre de paires de produits qui sont incompatible entre eux est inférieur à 50 et
-    //supérieur ou égal à 0. nb_inc sers aussi comme un compteur pour le tableau incomp et 
-    //exprime le fait que le compteur est positif et inférieur à 50 
+    //le nombre de paires de produits qui sont incompatible entre eux est inférieur à 50 et
+    //supérieur ou égal à 0. 
+    
     /*@ public invariant // Prop 2
       @ (0 <= nb_assign && nb_assign < 30);
       @*/
+    //le nombre d'affectation de produits(dans les batiments) s'élève à 30 et supérieur ou égal
+    //a 0. 
+    
     /*@ public invariant // Prop 3
       @ (\forall int i; 0 <= i && i < nb_inc; 
       @         incomp[i][0].startsWith("Prod") && incomp[i][1].startsWith("Prod"));
       @*/
+    //Tous les paires de produits(qui sont incompatible) ont un 
+    //nom qui commence par "Prod"
+    
     /*@ public invariant // Prop 4
       @ (\forall int i; 0 <= i && i < nb_assign; 
       @         assign[i][0].startsWith("Bat") && assign[i][1].startsWith("Prod"));
       @*/
+    //Les noms des batiments commencent par "Bat" et les produits sont assigné 
+    //commence par "Prod".
+    
     /*@ public invariant // Prop 5
       @ (\forall int i; 0 <= i && i < nb_inc; !(incomp[i][0]).equals(incomp[i][1]));
       @*/
+    //un produit ne peut pas etre incompatible avec lui-meme.
+    
     /*@ public invariant // Prop 6
       @ (\forall int i; 0 <= i && i < nb_inc; 
       @        (\exists int j; 0 <= j && j < nb_inc; 
       @           (incomp[i][0]).equals(incomp[j][1]) 
       @              && (incomp[j][0]).equals(incomp[i][1]))); 
       @*/
+    //Pour un pair de produit A et B incompatible, on a forcement un le paire B et A incompatible
+    
     /*@ public invariant // Prop 7
       @ (\forall int i; 0 <= i &&  i < nb_assign; 
       @     (\forall int j; 0 <= j && j < nb_assign; 
@@ -41,6 +53,7 @@ public class Explosives{
       @           (!(assign[i][1]).equals(incomp[k][0])) 
       @              || (!(assign[j][1]).equals(incomp[k][1])))));
       @*/
+    //Pour un batiment, on ne peut pas assigner deux produits qui sont incompatibles entre eux.
 
 
     public void add_incomp(String prod1, String prod2){
